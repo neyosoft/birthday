c2e91f0f3ea76b9f553064b882491bd209605883
Obagunwa Emmanuel <access2emma@gmail.com>
2021-06-12 11:17:31 +0100

    enable sending sms.. Temp remove changing of profile picture...

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -138,16 +138,13 @@ export const Profile = ({ navigation }) => {
             </TouchableOpacity>
             <AppText style={styles.title}>Profile</AppText>
 
-            {/* <TouchableOpacity style={styles.avatarWrapper} onPress={handleImageUpload}>
+            <TouchableOpacity style={styles.avatarWrapper}>
                 {profileImage ? (
                     <Image source={{ uri: profileImage }} style={{ width: 60, height: 60 }} />
                 ) : (
                     <UserAvatarIcon />
                 )}
-            </TouchableOpacity> */}
-            <View style={styles.avatarWrapper}>
-                <UserAvatarIcon />
-            </View>
+            </TouchableOpacity>
 
             <AppText style={styles.username}>
                 {user.givenName} {user.familyName}
