56303074ea8dfd63e8afa5f7cf568cb31cc68d26
Obagunwa Emmanuel <access2emma@gmail.com>
2021-11-08 00:08:54 +0100

    expo-camera not working

--- src/screens/main/ThankYouVideo.js
+++ src/screens/main/ThankYouVideo.js
@@ -1,15 +1,27 @@
-import React, { useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { useIsFocused } from "@react-navigation/core";
+import { Camera } from "expo-camera";
 
 import { theme } from "../../theme";
 import { useAuth } from "../../context";
+import { AppText } from "../../components";
 
 import { RFPercentage } from "react-native-responsive-fontsize";
 
 export const ThankYouVideo = ({ navigation }) => {
     const cameraRef = useRef();
     const isFocused = useIsFocused();
+
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
 
     const startRecording = async () => {
         try {
@@ -21,16 +33,40 @@ export const ThankYouVideo = ({ navigation }) => {
         }
     };
 
-    if (!isFocused) return null;
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
 
-    return <View style={StyleSheet.absoluteFill} isActive={true} />;
+    return (
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back,
+                            );
+                        }}>
+                        <Text style={styles.text}> Flip </Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 20,
-        backgroundColor: theme.backgroundColor,
+        backgroundColor: "#fff",
+        // backgroundColor: theme.backgroundColor,
     },
     backBtnArea: {
         padding: 25,
@@ -54,5 +90,23 @@ const styles = StyleSheet.create({
     btnText: {
         color: "#fff",
         fontSize: RFPercentage(1.8),
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        margin: 20,
+    },
+    button: {
+        flex: 0.1,
+        alignSelf: "flex-end",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 18,
+        color: "white",
     },
 });
